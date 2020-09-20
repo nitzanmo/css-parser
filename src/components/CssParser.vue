3d8f0e14a6ba5987160f8c3e3f8a67183d3aa869
nitzanmo <nitzan@supertools.ai>
2020-09-20 18:43:32 +0300

    :sparkles: added conversion from object syntax to classic :recycle: separated logic to methods

--- src/components/CssParser.vue
+++ src/components/CssParser.vue
@@ -23,11 +23,11 @@
           @click="handleDirectionChange"
           :class="[fromClassic ? 'arrow' : 'arrow-inverted']"/>
       <span id="disabled-wrapper">
-        <b-button variant="primary" @click="convert"
-                  :disabled="!fromClassic || (fromClassic && classicCss === '' || !fromClassic && objectCss === '')">
+        <b-button variant="primary" @click="convert">
+<!--                  :disabled="!fromClassic || (fromClassic && classicCss === '' || !fromClassic && objectCss === '')">-->
           Convert
         </b-button>
-        <b-tooltip target="disabled-wrapper" v-if="!fromClassic">Coming Soon</b-tooltip>
+        <!--        <b-tooltip target="disabled-wrapper" v-if="!fromClassic">Coming Soon</b-tooltip>-->
       </span>
     </div>
   </div>
@@ -39,7 +39,12 @@ export default {
   data() {
     return {
       classicCss: "",
-      objectCss: "",
+      objectCss: "className: {\n" +
+          "\twidth: 20,\n" +
+          "\theight:20,\n" +
+          "\tbackgroundColor: 'red',\n" +
+          "\toverflowY: 'hidden'\n" +
+          "}",
       fromClassic: sessionStorage.getItem('fromClassic') ? sessionStorage.getItem('fromClassic') === "True" : true,
     }
   },
@@ -57,67 +62,152 @@ export default {
     isNumeric(n) {
       return !isNaN(parseFloat(n)) && isFinite(n);
     },
+    convertFromClassic() {
+      let objectCss = '';
+      for (let i = 0; i < this.classicCss.length; i++) {
+        const previousChar = this.classicCss[i - 1];
+        const nextChar = this.classicCss[i + 1];
+        const char = this.classicCss[i];
+        const endOfLineIndex = this.classicCss.indexOf(';', i);
+        
+        if (char === '.' && (!previousChar || previousChar === '\n')) {
+          // converts class selectors to classes by removing the .
+        } else if (char === '-') {
+          // removes hyphens and applies camelCase
+          objectCss += nextChar.toUpperCase();
+          i++;
+          // converts ; to ,
+        } else if (char === ';') {
+          objectCss += ',';
+          // inserts ' before string values when : is found
+        } else if (char === ':') {
+          if (nextChar === ' ') {
+            objectCss += char;
+          } else {
+            // width:20px
+            if ((this.isNumeric(nextChar) || nextChar === '-') && this.isSinglePixelValue(this.classicCss, i)) {
+              objectCss += ' ' + this.classicCss.substr(i + 1, endOfLineIndex - 3 - i);
+              i = endOfLineIndex - 1;
+            } else {
+              // color:red -> color: 'red'
+              objectCss += ' \'' + this.classicCss.substr(i + 1, endOfLineIndex - 1 - i);
+              objectCss += '\'';
+              i = endOfLineIndex - 1;
+            }
+          }
+        } else if (char === ' ') {
+          if (nextChar === ' ') {
+            objectCss += ' ';
+          }
+          // width: 20px
+          if (previousChar === ':') {
+            if (this.isNumeric(nextChar) && this.isSinglePixelValue(this.classicCss, i)) {
+              objectCss += ' ' + this.classicCss.substr(i + 1, endOfLineIndex - 3 - i);
+              i = endOfLineIndex - 1;
+            } else {
+              // color: red -> color: 'red'
+              objectCss += ' \'' + this.classicCss.substr(i + 1, endOfLineIndex - 1 - i);
+              objectCss += '\'';
+              i = endOfLineIndex - 1;
+            }
+            // .bla { => bla: {
+          } else if (nextChar === '{') {
+            objectCss += ': ';
+          }
+        } else {
+          objectCss += char;
+        }
+      }
+      
+      this.objectCss = objectCss;
+    },
+    convertFromObject() {
+      let classicCss = '';
+      // className: {
+      //    width: 20 -> width: 20px,
+      //    height:20,
+      //    backgroundColor: 'red',
+      //    overflowY: 'hidden'
+      // },<\n>
+      // className: {
+      //    width: 20,
+      //    height:20,
+      //    backgroundColor: 'red',
+      //    overflowY: 'hidden'
+      // },
+      for (let i = 0; i < this.objectCss.length; i++) {
+        const previousChar = this.objectCss[i - 1];
+        const nextChar = this.objectCss[i + 1];
+        const char = this.objectCss[i];
+        const endOfLineIndex =
+            this.objectCss.indexOf(',', i) !== -1 ? this.objectCss.indexOf(',', i) :
+                this.objectCss.indexOf('\n', i) !== -1 ? this.objectCss.indexOf('\n', i) : this.objectCss.indexOf('}', i);
+        
+        /// object -> .object
+        if ((i === 0) || (i > 4 && (this.objectCss[i - 3] === '}' || this.objectCss[i - 2] === '}'))) {
+          classicCss += '.' + char;
+        } else if (char === ',') {
+          // color: 'red', -> color: red;
+          classicCss += ';';
+        } else if (char === '\n' && nextChar === '}') {
+          // color: 'red' -> color : red;
+          // }               }
+          if (previousChar !== ',') {
+            classicCss += ';' + char;
+          } else {
+            classicCss += char;
+          }
+        } else if (char === ':') {
+          // objectClass:{ -> objectClass {
+          if (nextChar === '{') {
+            classicCss += ' {';
+            i++;
+          } else if (nextChar === ' ' && this.objectCss[i + 2] === '{') {
+            classicCss += ' {';
+            i += 2;
+          } else if (this.isNumeric(nextChar)) {
+            // width:20, -> width: 20px;
+            classicCss += char + " " + this.objectCss.substr(i + 1, endOfLineIndex - i - 1) + "px";
+            i = endOfLineIndex - 1;
+          } else if (nextChar === '\'') {
+            // color:'red', -> color: red;
+            classicCss += " " + this.objectCss.substr(i, endOfLineIndex - i).split('\'')[1]
+            i = endOfLineIndex - 1;
+          } else {
+            classicCss += char;
+          }
+        } else if (char === ' ') {
+          if (nextChar === ' ') {
+            // trim double spaces
+          } else if (previousChar === ':') {
+            // width: 20, -> width: 20px;
+            if (this.isNumeric(nextChar)) {
+              // width: 20, -> width: 20px;
+              classicCss += this.objectCss.substr(i, endOfLineIndex - i) + "px";
+              i = endOfLineIndex - 1;
+            } else if (nextChar === '\'') {
+              // color:'red', -> color: red;
+              classicCss += " " + this.objectCss.substr(i, endOfLineIndex - i).split('\'')[1]
+              i = endOfLineIndex - 1;
+            } else {
+              classicCss += char;
+            }
+          }
+        } else if (!/\s/.test(char) && char !== '}' && char === char.toUpperCase()) {
+          // objectClass -> object-class
+          classicCss += '-' + char.toLowerCase();
+        } else {
+          classicCss += char;
+        }
+      }
+      
+      this.classicCss = classicCss;
+    },
     convert() {
       if (this.fromClassic) {
-        let objectCss = '';
-        for (let i = 0; i < this.classicCss.length; i++) {
-          const previousChar = this.classicCss[i - 1];
-          const nextChar = this.classicCss[i + 1];
-          const char = this.classicCss[i];
-          const endOfLineIndex = this.classicCss.indexOf(';', i);
-          
-          if (char === '.' && (!previousChar || previousChar === '\n')) {
-            // converts class selectors to classes by removing the .
-          } else if (char === '-') {
-            // removes hyphens and applies camelCase
-            objectCss += nextChar.toUpperCase();
-            i++;
-            // converts ; to ,
-          } else if (char === ';') {
-            objectCss += ',';
-            // inserts ' before string values when : is found
-          } else if (char === ':') {
-            if (nextChar === ' ') {
-              objectCss += char;
-            } else {
-              // width:20px
-              if ((this.isNumeric(nextChar) || nextChar === '-') && this.isSinglePixelValue(this.classicCss, i)) {
-                objectCss += ' ' + this.classicCss.substr(i + 1, endOfLineIndex - 3 - i);
-                i = endOfLineIndex - 1;
-              } else {
-                // color:red -> color: 'red'
-                objectCss += ' \'' + this.classicCss.substr(i + 1, endOfLineIndex - 1 - i);
-                objectCss += '\'';
-                i = endOfLineIndex - 1;
-              }
-            }
-          } else if (char === ' ') {
-            if (nextChar === ' ') {
-              objectCss += ' ';
-            }
-            // width: 20px
-            if (previousChar === ':') {
-              if (this.isNumeric(nextChar) && this.isSinglePixelValue(this.classicCss, i)) {
-                objectCss += ' ' + this.classicCss.substr(i + 1, endOfLineIndex - 3 - i);
-                i = endOfLineIndex - 1;
-              } else {
-                // color: red -> color: 'red'
-                objectCss += ' \'' + this.classicCss.substr(i + 1, endOfLineIndex - 1 - i);
-                objectCss += '\'';
-                i = endOfLineIndex - 1;
-              }
-              // .bla { => bla: {
-            } else if (nextChar === '{') {
-              objectCss += ': ';
-            }
-          } else {
-            objectCss += char;
-          }
-        }
-        
-        this.objectCss = objectCss;
+        this.convertFromClassic();
       } else {
-        // Logic here
+        this.convertFromObject();
       }
     },
     tabber(event, isClassic) {
@@ -130,7 +220,8 @@ export default {
       event.target.value = textWithTab // required to make the cursor stay in place.
       event.target.selectionEnd = event.target.selectionStart = originalSelectionStart + 1
     },
-  },
+  }
+  ,
 }
 </script>
 
