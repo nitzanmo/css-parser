de20885777a8e56d11bc0df1c8bb82a9af140aaa
nitzanmo <nitzan@supertools.ai>
2020-09-20 12:36:06 +0300

    :rocket: fixing deployment files

--- deploy.sh
+++ deploy.sh
@@ -9,14 +9,11 @@ npm run build
 # navigate into the build output directory
 cd dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
-
 git init
 git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+git push -f git@github.com:nitzanmo/css-parser.git master:gh-pages
 
 cd -
